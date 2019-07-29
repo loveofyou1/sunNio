722c560f482fc3436e0a0b226c0ae0df97256120
sunlei <sunwenlei9@126.com>
2019-07-29 22:28:49 +0800

    byteBuffer初体验

--- src/java/com/nio/one/TestNio.java
+++ src/java/com/nio/one/TestNio.java
@@ -1,10 +1,33 @@
 package com.nio.one;
 
+import sun.jvm.hotspot.runtime.Bytes;
+
+import java.nio.Buffer;
 import java.nio.ByteBuffer;
+import java.util.Arrays;
 
 public class TestNio {
     public static void main(String[] args) {
-        System.out.println("hello world!");
+        ByteBuffer buffer = ByteBuffer.allocate(10);
+        buffer.put((byte)'H').put((byte)'e').put((byte)'l').put((byte)'l').put((byte)'o');
+        buffer.put(0,(byte)'M').put((byte)'w');
 
+        //wrap method
+        byte[] arr = buffer.array();
+        ByteBuffer arrayBuffer = ByteBuffer.wrap(arr,0,1);
+    /*    for (byte arry : arrayBuffer.array()) {
+            char chr = (char) arry;
+            System.out.println(chr);
+        }*/
+
+        //flip翻转缓冲区，limit到缓冲区最后的位置
+        //buffer.flip();
+
+        buffer.rewind();//limit还是初始长度
+        byte[] bytes = new byte[buffer.limit()];
+        System.out.println(buffer.limit());
+        //System.out.println((char)buffer.get());
+        buffer.get(bytes);
+        System.out.println(Arrays.toString(bytes));
     }
 }
