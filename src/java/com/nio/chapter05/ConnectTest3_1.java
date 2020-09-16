8254a790b8a3d5bc04600495a8c52fc19e912f35
sunlei61 <sunlei19@jd.com>
2020-09-16 14:51:08 +0800

    复制缓冲区

--- src/java/com/nio/chapter05/ConnectTest3_1.java
+++ src/java/com/nio/chapter05/ConnectTest3_1.java
@@ -10,11 +10,12 @@ public class ConnectTest3_1 {
         SocketChannel socketChannel = null;
         try {
             socketChannel = SocketChannel.open();
+            // 非阻塞
             socketChannel.configureBlocking(false);
             System.out.println("是否连接状态:" + socketChannel.isConnectionPending());
             // ip地址不存在 192.1.1.1
             socketChannel.connect(new InetSocketAddress("192.1.1.1", 8088));
-            System.out.println("当前连接状态:"+socketChannel.isConnectionPending());
+            System.out.println("当前连接状态:" + socketChannel.isConnectionPending());
             socketChannel.close();
         } catch (Exception e) {
             e.printStackTrace();
